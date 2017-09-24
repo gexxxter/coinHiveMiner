c84bb696238c6cadcb163a07808b5bb9e63328b1
SlaxXx <basti_12@gmx.de>
2017-09-24 04:55:29 +0200

    login now returns userid

--- api/loginUser.php
+++ api/loginUser.php
@@ -1,21 +1,28 @@
+
 <?php
 include "dbConnect.php";
 $username  = $_GET['username'];
 $timeStamp = time();
-$stmt      = $con->prepare("SELECT * from user WHERE username=?");
+$stmt      = $con->prepare("SELECT id from user WHERE username=? LIMIT 1");
 $stmt->bind_param("s", $username);
+$response = array();
 $stmt->execute();
-$stmt->store_result();
-$count = $stmt->num_rows();
+$stmt->bind_result($userId);
+$stmt->fetch();
 $stmt->close();
-if ($count == 1) {
+if ($userId) {
     $stmt = $con->prepare("UPDATE user SET lastloggedin = $timeStamp WHERE username=?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
+    $response['userId'] = $userId;
+    $stmt->close();
 } else {
     $stmt = $con->prepare("INSERT INTO user (username,lastloggedin) VALUES (?,?)");
     $stmt->bind_param("si", $username, $timeStamp);
+    $userId = mysqli_stmt_insert_id ($stmt);
     $stmt->execute();
+    $response['userId'] = $stmt->insert_id;
+    $stmt->close();
 }
 
-$stmt->close();
+echo json_encode($response);
