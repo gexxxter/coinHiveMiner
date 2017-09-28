295dd9d92298c411822ef97e50438ac49ab4fb69
SlaxXx <basti_12@gmx.de>
2017-09-28 23:44:22 +0200

    Updated coinhive api url

--- api/checkConnections.php
+++ api/checkConnections.php
@@ -6,7 +6,7 @@ include "config.php";
 $curl = curl_init();
 curl_setopt_array($curl, array(
   CURLOPT_RETURNTRANSFER => 1,
-  CURLOPT_URL => 'https://api.coin-hive.com/stats/site?secret='.$coinHiveSecret
+  CURLOPT_URL => 'https://api.coinhive.com/stats/site?secret='.$coinHiveSecret
 ));
 
 $result = curl_exec($curl);
@@ -23,7 +23,7 @@ curl_close($curl);
 $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.coin-hive.com/user/top?secret='.$coinHiveSecret.'&count=10'
+    CURLOPT_URL => 'https://api.coi-hive.com/user/top?secret='.$coinHiveSecret.'&count=10'
 ));
 $result = curl_exec($curl);
 if($result === false){
--- api/getSiteStats.php
+++ api/getSiteStats.php
@@ -4,7 +4,7 @@ include "config.php";
   $curl = curl_init();
   curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.coin-hive.com/stats/site?secret='.$coinHiveSecret
+    CURLOPT_URL => 'https://api.coinhive.com/stats/site?secret='.$coinHiveSecret
   ));
   $result = curl_exec($curl);
   echo $result;
