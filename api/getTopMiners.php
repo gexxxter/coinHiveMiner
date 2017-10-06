1320a175290789e51203f10cf457a6e15fdd1a31
sinnatrix <31360025+sinnatrix@users.noreply.github.com>
2017-10-06 03:58:27 +0200

    Update getTopMiners.php

--- api/getTopMiners.php
+++ api/getTopMiners.php
@@ -4,7 +4,7 @@ $response = array();
 $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.coinhive.com/user/top?secret='.$coinHiveSecret.'&count=10'
+    CURLOPT_URL => 'https://api.coinhive.com/user/top?secret=Put ur Secret Key'
 ));
 $result = curl_exec($curl);
 $json = json_decode($result,true);
