e75bca81fdd0075aa125475a9a54d3b4c6f77627
GexXxter <basti_12@gmx.de>
2017-10-31 00:50:42 +0100

    typo

--- scripts/miner.js
+++ scripts/miner.js
@@ -81,7 +81,7 @@ $(function() {
           weeklyChart.data.datasets[0].data.push(data['hashesPerSecond']);
           var date = new Date(data['time'] * 1000);
           var label = date.getDate() + "." + (date.getMonth() + 1);
-          if (weeklyChart.data.labels.lenth == 0 || !$.inArray(label, weeklyChart.data.labels)) {
+          if (weeklyChart.data.labels.length == 0 || !$.inArray(label, weeklyChart.data.labels)) {
             weeklyChart.data.labels.push(label);
           }else{
             weeklyChart.data.labels.push("");
